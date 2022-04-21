a8fda35b7c7be3b24dbc55b1ffc329484be25aa1
adameldredge <43678759+adameldredge@users.noreply.github.com>
2022-04-20 22:17:56 -0400

    Add files via upload

---
+++ AdjacencyList.h
@@ -0,0 +1,303 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include "../vcpkg/installed/x86-windows/include/cpr/cpr.h"
+#include "../vcpkg/installed/x86-windows/include/gumbo.h"
+#include <istream>
+#include <vector>
+#include <map>
+#include <string>
+#include <stack>
+#include "AdjacencyList.h"
+#include "WebScraper.h"
+using namespace std;
+
+struct AdjacencyList
+{
+    struct Person
+    {
+        Person()
+        {
+            name = "";
+            id = 0;
+            visited = false;
+            identified = false;
+        }
+        Person(string n)
+        {
+            name = n;
+            id = 0;
+            visited = false;
+            identified = false;
+        }
+        Person operator=(const Person& d)
+        {
+            name = d.name;
+            id = d.id;
+            visited = d.visited;
+            identified = d.identified;
+            return*this;
+        }
+        bool operator==(const Person& d)
+        {
+            return ((d.name == name) && (d.adjNodes == adjNodes) && (d.id == id));
+        }
+        void setProfileLink(string l) {
+            int index = l.find("nm") + 2;
+            try {
+                this->id = stoi(l.substr(index, 7));
+                this->profileLink = l;
+            }
+            catch (std::invalid_argument) {}
+        }
+        void setID(unsigned int i)
+        {
+            id = i;
+        }
+        unsigned int getID()
+        {
+            return id;
+        }
+        void addAdj(Person* n)
+        {
+            adjNodes.push_back(n);
+        }
+        vector<Person*> getAdj()
+        {
+            return adjNodes;
+        }
+        string name;
+        string profileLink;
+        unsigned int id;
+        bool visited;
+        bool identified;
+        vector<Person*> adjNodes;
+    };
+
+    // Member Variables
+    Person* root;
+    int size;
+    map<string, Person*> nodes;
+    WebScraper w;
+
+    // Functions
+    AdjacencyList()
+    {
+        root = nullptr;
+        size = 0;
+        loopAddData();
+    }
+    Person* newPerson(string name)
+    {
+        Person* p = new Person(name);
+        p->id = nodes.size();
+        this->size++;
+        if (root == nullptr)
+            root = p;
+        nodes.emplace(name, p);
+        return p;
+    }
+    void addEdge(string srcName, string destName)
+    {
+        Person* tempsrc;
+        Person* tempdest;
+        if (nodes.find(srcName) == nodes.end())
+            tempsrc = newPerson(srcName);
+        else
+            tempsrc = nodes[srcName];
+        if (nodes.find(destName) == nodes.end())
+            tempdest = newPerson(destName);
+        else
+            tempdest = nodes[destName];
+        tempsrc->addAdj(tempdest);
+    }
+    Person* getPerson(string name, unsigned int id)
+    {
+        Person* temp = nodes[name];
+        if (!temp)
+            return nullptr;
+        if (temp->id == id)
+            return temp;
+    }
+    void recursiveAddEdges(unsigned int count, string name, bool& endRecursion) {
+        if (count >= 3) {
+            return;
+        }
+        if (size > 1000) {
+            //cout << "110,000 nodes!" << endl;
+            endRecursion = true;
+            return;
+        }
+        vector<string> moviesFromProfile;
+        bool killSwitch = endRecursion;
+        w.getProfileMovieLinks(nodes[name]->profileLink, moviesFromProfile);
+        vector<string> castAtIndex;
+        vector<string> profilesFromMovie;
+        for (unsigned int i = 0; i < moviesFromProfile.size(); i++) {
+            castAtIndex = w.getMovieRoster(moviesFromProfile.at(i), profilesFromMovie);
+            for (unsigned int j = 0; j < castAtIndex.size(); j++) {
+                if (killSwitch) {
+                    break;
+                }
+                addEdge(name, castAtIndex.at(j));
+                if (nodes[castAtIndex.at(j)]->profileLink == "") {
+                    nodes[castAtIndex.at(j)]->setProfileLink(profilesFromMovie.at(j));
+                }
+                recursiveAddEdges(count + 1, castAtIndex.at(j), endRecursion);
+            }
+            castAtIndex.clear();
+            if (killSwitch) {
+                break;
+            }
+        }
+        profilesFromMovie.clear();
+        moviesFromProfile.clear();
+
+    }
+    void loopAddData() {
+        vector<string> movieLinks; //links to all movies from topThousand at page i
+        vector<string> names; //list of names from the movie at j
+        vector<string> linksToProfiles; //links to actor's profile
+        bool killSwitch = false;
+        for (unsigned int i = 0; i < 11; i++) {
+            movieLinks = w.getLinksFromSearch(w.extractTopThousand(to_string(i + 1)));
+            if (killSwitch) { break; }
+            for (unsigned int j = 0; j < movieLinks.size(); j++) {
+                if (killSwitch) { break; }
+                names = w.getMovieRoster(movieLinks.at(j), linksToProfiles); //now have linkstoprofiles set
+                //we have all our names here, so go through names and add everyone in this movie as well as
+                //everyone in their other movies
+                for (unsigned int k = j + 1; k < names.size(); k++) {
+                    addEdge(names.at(j), names.at(k));
+                }
+                for (unsigned int p = 0; p < names.size(); p++) {
+                    if (nodes[names.at(p)]->profileLink == "") {
+                        nodes[names.at(p)]->profileLink = linksToProfiles.at(p);
+                    }
+                    recursiveAddEdges(0, names.at(p), killSwitch);
+                    if (killSwitch) { break; }
+                }
+                names.clear();
+            }
+            linksToProfiles.clear();
+            movieLinks.clear();
+        }
+    }
+
+
+    // Once Graph is fully made
+    // Dijkstra's
+    bool isAdjacent(AdjacencyList::Person* first, AdjacencyList::Person* second)
+    {
+        for (int i = 0; i < first->adjNodes.size(); i++)
+        {
+            if (first->adjNodes.at(i) == second)
+                return true;
+        }
+        return false;
+    }
+    vector<AdjacencyList::Person*> dijkstra(map<string, AdjacencyList::Person*> graph, AdjacencyList::Person* source, AdjacencyList::Person* Destination)
+    {
+        map<int, AdjacencyList::Person*> S;
+        map<int, AdjacencyList::Person*> VS;
+        map<AdjacencyList::Person*, int> d;
+        map<AdjacencyList::Person*, AdjacencyList::Person*> p;
+
+        // Add all People to VS
+        for (auto it : graph)
+        {
+            VS[it.second->id] = it.second;
+        }
+
+        // Move source person to S
+        S[source->id] = VS[source->id];
+        VS.erase(source->id);
+
+        // Set source as predecessor and max as distance (or 1 if connected to source)
+        for (auto it : VS)
+        {
+            if (isAdjacent(S[source->id], it.second))
+                d[it.second] = 1;
+            else
+                d[it.second] = INT_MAX;
+
+            p[it.second] = S[source->id];
+        }
+
+        // Iterate through VS and update distances and predecessors
+        while (!VS.empty())
+        {
+            AdjacencyList::Person* smallestDistance = VS.begin()->second;
+            for (auto it : VS)
+            {
+                if (d[it.second] < d[smallestDistance])
+                {
+                    smallestDistance = it.second;
+                }
+            }
+            S[smallestDistance->id] = VS[smallestDistance->id];
+            VS.erase(smallestDistance->id);
+            for (auto it : S[smallestDistance->id]->adjNodes)
+            {
+                if (d[smallestDistance] + 1 < d[it])
+                {
+                    d[it] = d[smallestDistance] + 1;
+                    p[it] = S[smallestDistance->id];
+                }
+            }
+        }
+
+        // Iterate through predecessors of destination until find source and output all through a vector
+        AdjacencyList::Person* currentPredecessor = p[Destination];
+        vector<AdjacencyList::Person*> path;
+        path.push_back(Destination);
+        while (currentPredecessor != source)
+        {
+            path.push_back(currentPredecessor);
+            currentPredecessor = p[currentPredecessor];
+        }
+        path.push_back(source);
+        // Flip vector
+        vector<AdjacencyList::Person*> updatedPath;
+        for (int i = path.size() - 1; i >= 0; i--)
+        {
+            updatedPath.push_back(path.at(i));
+        }
+        return updatedPath;
+    }
+
+    // BFS
+    vector<AdjacencyList::Person*> DFSvsBFS(map<string, AdjacencyList::Person*> graph)
+    {
+        // DFS
+        stack<AdjacencyList::Person*> s;
+        vector<AdjacencyList::Person*> pathDFS;
+        AdjacencyList::Person* start = graph.begin()->second;
+        start->identified = true;
+        s.push(start);
+        while (!s.empty())
+        {
+            AdjacencyList::Person* current = s.top();
+            pathDFS.push_back(current);
+            s.pop();
+            for (auto it : current->adjNodes)
+            {
+                if (!it->identified && !it->visited)
+                {
+                    it->identified = true;
+                    s.push(it);
+                }
+            }
+            current->visited = true;
+        }
+        // Go back through and mark all as non-visited or identified.
+        for (auto it : graph)
+        {
+            it.second->visited = false;
+            it.second->identified = false;
+        }
+        return pathDFS;
+    }
+};
+
---
+++ WebScraper.h
@@ -0,0 +1,217 @@
+#ifndef Webscraper
+#define Webscraper
+#endif
+
+#pragma once
+#include <iostream>
+#include <fstream>
+#include "../vcpkg/installed/x86-windows/include/cpr/cpr.h"
+#include "../vcpkg/installed/x86-windows/include/gumbo.h"
+#include <istream>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+struct WebScraper {
+    WebScraper() {
+        test = "";
+    }
+    //adds names of directors and writers to vector of names plus their profile links in a different vector from snippet of html
+    void getDirAndWriter(vector<string>& names, string& webInfo, vector<string>& profileLinks){
+        int linkFind, index2;
+        int distToTableEnd = INT_MAX;//webInfo.find("h4");
+        linkFind = 0;
+        linkFind = webInfo.find("a href=") + 9;
+        while (distToTableEnd > linkFind) {
+            webInfo = webInfo.substr(linkFind);
+            linkFind = webInfo.find("\"\n");
+            profileLinks.push_back("https://www.imdb.com/" + webInfo.substr(0, linkFind));
+            webInfo = webInfo.substr(linkFind + 4);
+            index2 = webInfo.find("\n<");
+            names.push_back(webInfo.substr(0, index2));
+            distToTableEnd = webInfo.find("ast_list");
+            linkFind = webInfo.find("a href=") + 9;
+            //distToTableEnd = webInfo.find("Cast");
+        }
+        //just to debug:
+        try {
+            webInfo = webInfo.substr(distToTableEnd);
+        }
+        catch (std::out_of_range) {
+            return;
+        }
+        //linkFind = webInfo.find("writer");
+        //webInfo = webInfo.substr(linkFind);
+        //linkFind = webInfo.find("table class=");
+        //webInfo = webInfo.substr(linkFind);
+        //distToTableEnd = webInfo.find("/table");
+        //while (distToTableEnd > linkFind) {
+        //    linkFind = webInfo.find("a href=") + 9;
+        //    webInfo = webInfo.substr(linkFind);
+        //    linkFind = webInfo.find('"');
+        //    profileLinks.push_back("https://www.imdb.com/" + webInfo.substr(0, linkFind));
+        //    webInfo = webInfo.substr(linkFind);
+        //    index2 = webInfo.find('>');
+        //    webInfo = webInfo.substr(index2);
+        //    if (webInfo[0] == ' ') {
+        //        webInfo = webInfo.substr(1);
+        //    }
+        //    index2 = webInfo.find(" </a>");
+        //    names.push_back(webInfo.substr(0, index2));
+        //    distToTableEnd = webInfo.find("/table");
+        //}
+    }
+    //takes inputs of a snippet of html code for movie site, vectors for names and links to profiles to be added to (just actors)
+    void getCastMembers(string& webInfo, vector<string>& names, vector<string>& profileLinks) {
+        /*int index = webInfo.find("table class=");
+        webInfo = webInfo.substr(index);*/
+        int index = webInfo.find("tr class=");
+        int endOfCast = webInfo.find("dataHeaderWithBorder");
+        while (index < endOfCast && index > 0) {
+            webInfo = webInfo.substr(index);
+            index = webInfo.find("a href=") + 9;
+            webInfo = webInfo.substr(index);
+            index = webInfo.find('\"');
+            profileLinks.push_back("https://www.imdb.com/" + webInfo.substr(0, index));
+            index = webInfo.find("title=") + 7;
+            webInfo = webInfo.substr(index);
+            index = webInfo.find('\"');
+            names.push_back(webInfo.substr(0, index));
+            index = webInfo.find("tr class=");
+            endOfCast = webInfo.find("dataHeaderWithBorder");
+        }
+        try {
+            webInfo = webInfo.substr(endOfCast);
+        }
+        catch (std::out_of_range) {
+            return;
+        }
+    }
+    //input is vector of names for crew to be added to (alr has director/writer and actors), snippet
+    //of html code, vector for profile page urls
+    void getCrew(vector<string>& names, string& webInfo, vector<string>& profileLinks) {
+        int bodyIndex = webInfo.find("tbody");
+        if (bodyIndex == -1) {
+            return;
+        }
+        webInfo = webInfo.substr(bodyIndex);
+        int rowIndex, linkPos, nameEnd;
+        rowIndex = webInfo.find("td class=");
+        string link = "https://www.imdb.com/";
+        while (rowIndex != string::npos) {
+            webInfo = webInfo.substr(rowIndex);
+            linkPos = webInfo.find("a href=") + 9;
+            webInfo = webInfo.substr(linkPos);
+            linkPos = webInfo.find('\"');
+            profileLinks.push_back(link + webInfo.substr(0, linkPos));
+            linkPos = webInfo.find("\n>") + 3;
+            webInfo = webInfo.substr(linkPos);
+            nameEnd = webInfo.find("</a>") - 1;
+            names.push_back(webInfo.substr(0, nameEnd));
+            rowIndex = webInfo.find("td class=");
+        }
+        names.pop_back();
+        profileLinks.pop_back();
+    }
+
+    void getProfileMovieLinks(string linkToProfile, vector<string>& movieLinks) {
+        string webInfo = cpr::Get(cpr::Url{ linkToProfile }).text;
+        unsigned int index = webInfo.find("filmo-category-section");
+        webInfo = webInfo.substr(index);
+        string linkToMovie;
+        unsigned int linkEndIndex;
+        unsigned int endIndex = INT_MAX;
+        endIndex = webInfo.find("archive_footage");
+        index = webInfo.find("a href=");
+        while (index < endIndex) {
+            webInfo = webInfo.substr(index + 8);
+            linkEndIndex = webInfo.find('\"');
+            linkToMovie = webInfo.substr(0, linkEndIndex);
+            movieLinks.push_back("https://www.imdb.com" + linkToMovie);
+            index = webInfo.find("filmo-row");
+            index = webInfo.find("a href=");
+        }
+    }
+    
+    vector<string> getMovieRoster(string& link, vector<string>& profileLinks) {
+        /*int test1, test2;
+        test1 = link.find("https://");
+        test2 = link.rfind("https://");
+        if (test1 != test2) {
+            link = link.substr(link.rfind("tt"));
+            int p = link.find("?");
+            link = link.substr(0, p);
+            link = "https://imdb.com/title/" + link;
+        }*/
+        
+        vector<string> retVal;
+        int test1 = link.find("pro.");
+        int test2 = link.find("help.");
+        int check3 = link.find("title");
+        if ((test1 != -1) || (test2 != -1) || (check3 == -1)) {
+            return retVal;
+        }
+        //vector<string> profileLinks;
+        string fullCastLinkEnd = "fullcredits?ref_=tt_cl_sm";
+        int index = link.find("?ref");
+        string linkToFullCast = link.substr(0, index); //make sure this goes up to the / after the /title/ part of link
+        if (linkToFullCast.back() != '/') {
+            linkToFullCast += '/';
+        }
+        linkToFullCast += fullCastLinkEnd;
+        string htmlCode;
+        try {
+            htmlCode = cpr::Get(cpr::Url{ linkToFullCast }).text;
+            index = htmlCode.find("ullcredits_content");
+        }
+        catch (exception e) { return retVal; }
+        int index2 = htmlCode.find("contribute-main-section");
+        try {
+            htmlCode = htmlCode.substr(index, index2); //all htmlCode for the cast/crew
+        }
+        catch (exception e) {
+            return retVal;
+        }
+        index = htmlCode.find("table class=");
+        htmlCode = htmlCode.substr(index);
+        getDirAndWriter(retVal, htmlCode, profileLinks);
+        getCastMembers(htmlCode, retVal, profileLinks);
+        getCrew(retVal, htmlCode, profileLinks);
+        //will have a list of the entire cast for this movie plus a link to their profile at the same index
+        //
+        return retVal;
+
+    }
+    string extractTopThousand(string pageNum) {
+        string link = "https://www.imdb.com/list/ls009131936/";
+        if (pageNum != "1") {
+            link += "?page=" + pageNum;
+        }
+        cpr::Url url = cpr::Url { link };
+        string response = cpr::Get(url).text;
+        return response;
+    }
+    vector<string> getLinksFromSearch(string webInfo) {
+        vector<string> retVal;
+        string linkKey = "lister-list";
+        string linkBase = "https://www.imdb.com";
+        //string linkEnd = "fullcredits/?ref_=tt_cl_sm";
+        int index = webInfo.find(linkKey);
+        int distToLinkEnd = 0;
+        string htmlCode = webInfo.substr(index);
+        linkKey = "lister-item-content";
+        index = webInfo.find(linkKey);
+        while (index != string::npos) {
+            htmlCode = htmlCode.substr(index);
+            htmlCode = htmlCode.substr(htmlCode.find("lister-item-index unbold text-primary"));
+            htmlCode = htmlCode.substr(htmlCode.find("href="));
+            htmlCode = htmlCode.substr(htmlCode.find('/'));
+            distToLinkEnd = htmlCode.find('"');
+            retVal.push_back(linkBase + htmlCode.substr(0, distToLinkEnd));
+            index = htmlCode.find(linkKey);
+        }
+        return retVal;
+    }
+    string test;
+};
